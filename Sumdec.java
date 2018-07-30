dd7bc74684a9cc5472fec103e5e290bc30b59f6e
skjanani <41111202+skjanani@users.noreply.github.com>
2018-07-30 14:51:05 +0530

    Add files via upload

---
+++ Sumdec.java
@@ -0,0 +1,20 @@
+package janani;
+import java.util.Scanner;
+public class Sumdec {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i,n;
+String str;
+System.out.println("enter the string");
+Scanner ja=new Scanner(System.in);
+str=ja.next();
+n=ja.nextInt();
+for(i=0;i<n;i++){
+	//sum=sum+i;
+	System.out.println(str);
+}
+System.out.println();
+	}
+
+}
